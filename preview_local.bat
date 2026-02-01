b528a68e67a39cb95d2e49951a778e711d3f1bbe
kentng111 <kentng111@gmail.com>
2026-02-01 19:03:17 +0100

    update site

--- preview_local.bat
+++ preview_local.bat
@@ -7,5 +7,13 @@ echo Hugo Local Server
 echo %cd%
 echo ==============================
 
+REM --- public フォルダをクリーン ---
+if exist public (
+  echo Cleaning public folder...
+  rmdir /s /q public
+)
+
+REM --- Hugo ローカルサーバー起動 ---
 hugo server --disableFastRender --buildDrafts
+
 pause
